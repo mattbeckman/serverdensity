567ba99c6f48b3a04c02352e02221ca9aac8872a
mattbeckman <matt@beckman.co>
2013-11-12 00:12:33 -0800

    Initial commit for NServiceBusStatus plugin

---
+++ Plugins/NServiceBusStatus/NServiceBusStatus.UnitTest/NServiceBusStatusFixture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ServiceProcess;
+using NUnit.Framework;
+
+namespace ServiceStatus.UnitTest
+{
+    [TestFixture]
+    public class NServiceBusStatusFixture
+    {
+        [Test]
+        public void Should_locate_all_NSB_services()
+        {
+            var nsbStatus = new NServiceBusStatus();
+
+            var results = (IDictionary<string, object>) nsbStatus.DoCheck();
+
+            Assert.AreEqual((int)ServiceControllerStatus.Running, results["Publisher"]);
+            Assert.AreEqual((int)ServiceControllerStatus.Running, results["Subscriber A"]);
+            Assert.AreEqual((int)ServiceControllerStatus.Running, results["Subscriber B"]);
+            Assert.AreEqual(results.Count, 3);
+        }
+    }
+}
---
+++ Plugins/NServiceBusStatus/NServiceBusStatus/NServiceBusStatus.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ServiceProcess;
+using BoxedIce.ServerDensity.Agent.PluginSupport;
+using Microsoft.Win32;
+
+namespace ServiceStatus
+{
+    public class NServiceBusStatus : ICheck
+    {
+        public string Key
+        {
+            get { return "NServiceBusStatus"; }
+        }
+
+        public object DoCheck()
+        {
+            var serviceNames = GetServiceNames();
+
+            return CheckServiceStatuses(serviceNames);
+        }
+
+        private IDictionary<string, object> CheckServiceStatuses(IList<string> serviceNames)
+        {
+            var nsbServiceStates = new Dictionary<string, object>();
+
+            foreach (var serviceName in serviceNames)
+            {
+                var service = new ServiceController(serviceName);
+                
+                nsbServiceStates.Add(service.DisplayName, (int) service.Status);
+            }
+
+            return nsbServiceStates;
+        }
+
+        private IList<string> GetServiceNames()
+        {
+            var serviceNames = new List<string>();
+
+            using (var servicesKey = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Services"))
+            {
+                foreach (var serviceKey in servicesKey.GetSubKeyNames())
+                {
+                    using (var registryKey = servicesKey.OpenSubKey(serviceKey))
+                    {
+                        if (registryKey != null)
+                        {
+                            var imagePath = registryKey.GetValue("ImagePath");
+
+                            if (imagePath != null && imagePath.ToString().Contains("NServiceBus.Host.exe"))
+                                serviceNames.Add(serviceKey);
+                        }
+                    }
+                }
+            }
+
+            return serviceNames;
+        }
+    }
+}
