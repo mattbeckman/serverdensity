9ea6b66a8d4e648a9c6bfb201c26ece3a542e64b
mattbeckman <matt@beckman.co>
2013-11-13 01:41:07 -0800

    Initial commit for Uptime plugin

---
+++ Plugins/Uptime/Uptime.UnitTest/UptimeFixture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Uptime.UnitTest
+{
+    [TestFixture]
+    public class UptimeFixture
+    {
+        [Test]
+        public void Should_retrieve_uptime_from_local_system()
+        {
+            var uptime = new Uptime();
+
+            var result = (IDictionary<string, double>) uptime.DoCheck();
+
+            Assert.That(result["Total Seconds"] > 0);
+            Assert.That(result["Total Minutes"] > 0);
+            Assert.That(result["Total Hours"] > 0);
+            Assert.That(result["Total Days"] > 0);
+        }
+    }
+}
---
+++ Plugins/Uptime/Uptime/Uptime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BoxedIce.ServerDensity.Agent.PluginSupport;
+
+namespace Uptime
+{
+    public class Uptime : ICheck
+    {
+        public string Key
+        {
+            get { return "Uptime"; }
+        }
+
+        public object DoCheck()
+        {
+            var result = new Dictionary<string, double>();
+
+            using (var uptime = new PerformanceCounter("System", "System Up Time"))
+            {
+                uptime.NextValue(); // initial value always zero
+                var uptimeValue = uptime.NextValue();
+
+                result.Add("Total Seconds", TimeSpan.FromSeconds(uptimeValue).TotalSeconds);
+                result.Add("Total Minutes", TimeSpan.FromSeconds(uptimeValue).TotalMinutes);
+                result.Add("Total Hours", TimeSpan.FromSeconds(uptimeValue).TotalHours);
+                result.Add("Total Days", TimeSpan.FromSeconds(uptimeValue).TotalDays);
+            }
+
+            return result;
+        }
+    }
+}
